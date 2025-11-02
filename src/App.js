e39543b86ed01414c1ddaac4191a351b74475717
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2025-11-02 07:20:05 +0200

    Updated App.js
    Added Dictionary.js and Dictionary.css files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import logo from "./logo195.jpg";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo}
-        className="App-logo" 
+        className="App-logo img-fluid"
         alt="logo"  />
     
       </header>
+    <main>
+    <Dictionary/>
+     </main>
+      <footer className="App-footer"> 
+        <small>Coded by tich tach</small>
+        </footer>
+      </div>
     </div>
+
   );
 }
 
